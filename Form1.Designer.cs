976a266bb22d5df079bd9b4f2558b396157d14e2
Alex <argentum.tm@gmail.com>
2019-03-02 16:18:44 +0300

    Added field generation

---
+++ Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace wave_alg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.statusBar1 = new System.Windows.Forms.StatusBar();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(396, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Сгенерировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 53);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(162, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "X";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(185, 17);
+            this.textBox2.MaxLength = 2;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(56, 17);
+            this.textBox1.MaxLength = 2;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(35, 35);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "15";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Location = new System.Drawing.Point(12, 71);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(600, 540);
+            this.panel2.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Yellow;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(35, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(35, 35);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "22";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Visible = false;
+            // 
+            // statusBar1
+            // 
+            this.statusBar1.Location = new System.Drawing.Point(0, 619);
+            this.statusBar1.Name = "statusBar1";
+            this.statusBar1.Size = new System.Drawing.Size(624, 22);
+            this.statusBar1.TabIndex = 3;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(408, 619);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(160, 20);
+            this.progressBar1.TabIndex = 4;
+            this.progressBar1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 641);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.statusBar1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Wave alg";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.StatusBar statusBar1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wave_alg
+{
+    public partial class Form1 : Form
+    {
+        int width;
+        int height;//размеры матрицы MxN
+        int distance_x;
+        int distance_y;//расстояние между кнопками
+        Button[,] button_array = new Button[1000,1000];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            statusBar1.Text = "Укажите размеры платы";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            width = Convert.ToInt32(textBox1.Text);
+            height = Convert.ToInt32(textBox2.Text); //читаем размеры матрицы из texBox'ов 
+            // вычисление размеров кнопок
+            panel2.Width = ((panel2.Width / width) + 1) * width;
+            panel2.Height = ((panel2.Height / height) + 1) * height;
+            distance_x = (panel2.Width/width);
+            distance_y = (panel2.Height/height); 
+            Console.Write(panel2.Width + " " + panel2.Height);
+            Console.WriteLine(distance_x+" "+distance_y);
+            //прогресс-бар
+            progressBar1.Visible = true;
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = width;
+            progressBar1.Value = 0;
+            progressBar1.Step = 1;
+            for (int x = 0;(x)<width*distance_x; x += distance_x)
+            {
+                for (int y = 0; (y ) < height * distance_y; y += distance_y)
+                {
+                    create_Cell(x, y);
+                }
+                progressBar1.PerformStep();
+            }
+            progressBar1.Visible=false;
+            button1.Visible = false;
+            //this.Width = (distance_x * width) + 40;
+            //this.Height = (distance_y * height) + 140;
+            this.Width = panel2.Width +40;
+            this.Height =panel2.Height + 140;
+            statusBar1.Text = "Выберите стартовую позицию";
+            button_array[1, 1].BackColor = Color.Orange;
+
+        }
+        // функция генерации ячейки
+        private void create_Cell(int x, int y)
+        {
+            Button button = new Button();
+            button.Location = new Point(x, y); //позиция кнопки 
+            button.Size = new Size(distance_x, distance_y); //размеры
+            button.FlatAppearance.BorderSize = 1; 
+            button.FlatAppearance.BorderColor = Color.Silver;  //рамка
+            button.FlatStyle = FlatStyle.Flat; // плоская кнопка
+            panel2.Controls.Add(button); // добавление на форму
+            button.Click += new EventHandler(onCellClick); //добавляем обработчик клика
+            button_array[(x / distance_x), (y / distance_y)] = button; // заполняем массив кнопками, 
+                                                                   // чтобы к ним можно было получить доступ в любое время           
+        }
+        //функция-обработчик клика на ячейку
+        void onCellClick(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            //тут будет много кода 
+            Console.WriteLine("good");
+            Console.WriteLine(button.Location);
+            Console.WriteLine(button.Size);
+            Console.WriteLine(this.Width);
+            Console.WriteLine(this.Height);
+        }
+    }
+
+}
